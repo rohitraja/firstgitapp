5a9439ec4ce070b16aba9eb7a8dce9b7fe40d80c
Veron <rohit.raja56@gmail.com>
2014-09-28 16:38:11 +0530

    Test for Postgres connection

---
+++ src/main/java/com/example/models/ConnectionDto.java
@@ -0,0 +1,28 @@
+package com.example.models;
+
+public class ConnectionDto {
+
+	private String userName;
+	private String pwd;
+	private String responce;
+	
+	public String getResponce() {
+		return responce;
+	}
+	public void setResponce(String responce) {
+		this.responce = responce;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+}
--- src/main/java/com/example/services/TimeService.java
+++ src/main/java/com/example/services/TimeService.java
@@ -1,8 +1,16 @@
 package com.example.services;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.example.models.ConnectionDto;
 import com.example.models.Time;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -17,5 +25,36 @@ public class TimeService {
         return new Time();
     }
 
+    @POST
+    @Path("/createconnection")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public ConnectionDto createconnection(ConnectionDto conDto)
+    {
+    	Connection con;
+    	Statement stmt;
+	    try {
+			String url = 
+			       "jdbc:postgresql://reddwarf.cs.rit.edu/rkr";
+			    // DATABASE CONNECTION MAGIC :-)
+			    Class.forName("org.postgresql.Driver");
+			    con = DriverManager.getConnection(url, conDto.getUserName(), conDto.getPwd());
+			    stmt = con.createStatement();
+				conDto.setResponce("Successfully Connection created");
+
+			    
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			conDto.setResponce("Class no found Exception :"+e.toString());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			conDto.setResponce("SQLException e :"+e.toString());
+
+		}
+    	
+    	return conDto;
+    }
 }
 
